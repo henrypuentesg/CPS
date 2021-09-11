7f60b6fc772b73d91f7895436339a321f382b0b5
Henry Puentes Gonzalez <henry.puentes@externos-co.cencosud.com>
2021-09-11 17:09:05 -0500

    Se actualizan template y se una backEnd con frontEnd

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,17 +1,4 @@
 from django.shortcuts import render, HttpResponse
-
-html_base = """
-
-<h1>mi web 1</h1>
-<ul>
-    <li><a href="/">portada </a> </li>
-    <li><a href="/about">about-me</a> </li>
-    <li><a href="/portafolio">Portafolio</a> </li>
-    <li><a href="/contacto">Contacto</a> </li>
-</ul>
-
-"""
-
 # Create your views here.
 def home(request):
     return render(request,"core/home.html")
@@ -23,4 +10,4 @@ def portafolio(request):
     return render(request,"core/portafolio.html")
 
 def contacto(request):
-    return HttpResponse(html_base + "<h1>mi web 1</h1><h2>Contacto</h2><p>Contacto</p>")
+    return render(request,"core/contacto.html")
